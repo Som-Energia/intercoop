85cabe5c1a4e09829b1603acb901662527ab6369
David García Garzón <david.garcia@upf.edu>
2016-06-24 21:36:36 +0200

    lets talk about text (instead unicode string)

--- python/intercoop/crypto.py
+++ python/intercoop/crypto.py
@@ -7,25 +7,25 @@ class _deps:
     from Crypto.PublicKey import RSA
     from Crypto.Signature import PKCS1_v1_5
 
-def encode(payload):
-    "Encode a unicode string into base64 (as unicode string)"
-    return bencode(payload.encode('utf8'))
+def encode(text):
+    "Encode text into base64 (as text)"
+    return bencode(text.encode('utf8'))
 
-def decode(encodedPayload):
-    "Decode utf8 base64 into unicode string"
-    return bdecode(encodedPayload).decode('utf8')
+def decode(b64string):
+    "Decode base64 (as text) back into text"
+    return bdecode(b64string).decode('utf8')
 
-def bencode(binaryPayload):
-    "Encode bytes as base64 (as unicode string)"
-    return _deps.base64.urlsafe_b64encode(binaryPayload).decode('utf8')
+def bencode(binaryStream):
+    "Encode bytes (binary) as base64 (as text)"
+    return _deps.base64.urlsafe_b64encode(binaryStream).decode('utf8')
 
 def bdecode(b64string):
-    "Decode a unicode string representing base64 stream into bytes"
+    "Decode base64 (as text) back into bytes (binary)"
     return _deps.base64.urlsafe_b64decode(b64string.encode('utf8'))
 
-def sha(payload):
-    "Returns the SHA of an unicode string encoded as utf-8"
-    return _deps.SHA.new(payload.encode('utf-8'))
+def sha(text):
+    "Returns the SHA of a text"
+    return _deps.SHA.new(text.encode('utf-8'))
 
 def generateKeyPair(filename, publicfilename=None):
     key = _deps.RSA.generate(2048)
@@ -43,13 +43,13 @@ def exportPublicKey(key, filename):
     with open(filename,'wb') as f:
         f.write(public.exportKey('PEM'))
 
-def sign(privatekey, payload):
+def sign(privatekey, text):
     signer = _deps.PKCS1_v1_5.new(privatekey)
-    return bencode(signer.sign(sha(payload)))
+    return bencode(signer.sign(sha(text)))
 
-def isAuthentic(publickey, payload, signature):
+def isAuthentic(publickey, text, signature):
     verifier = _deps.PKCS1_v1_5.new(publickey)
     decodedSignature = bdecode(signature)
-    return verifier.verify(sha(payload), decodedSignature)
+    return verifier.verify(sha(text), decodedSignature)
 
 # vim: ts=4 sw=4 et
