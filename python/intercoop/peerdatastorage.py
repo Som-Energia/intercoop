e51fc98376e07ef7647b0ef94cff1d73e78977ad
David García Garzón <david.garcia@upf.edu>
2016-06-30 11:12:58 +0200

    working tests

--- python/intercoop/peerdatastorage.py
+++ python/intercoop/peerdatastorage.py
@@ -1,11 +1,15 @@
 # -*- encoding: utf-8 -*-
 
+from yamlns import namespace as ns
+import os
+
 class PeerDataStorage(object):
     
     def __init__(self,datadir):
         self.datadir=datadir
 
     def get(self,peername):
-        
+        return ns.load(os.path.join(self.datadir, peername+'.yaml'))
+
 
 # vim: ts=4 sw=4 et
--- python/intercoop/peerdatastorage_test.py
+++ python/intercoop/peerdatastorage_test.py
@@ -4,21 +4,47 @@ from . import peerdatastorage
 from yamlns import namespace as ns
 
 """
-    - Hacer el setUp
-    - Get cuando funciona
-    - Get cuando no está
-    - Iterador
+# TODO's
+
+- [x] Hacer el setUp
+- [x] Get cuando funciona
+- [ ] Get cuando no está
+- [ ] Proteger contra peerid's maliciosos (../../) limitando a letras numeros y signos de separacion validos
+- [ ] Iterador
+- [ ] Accesores mas semanticos
 """
+
+somacmeyaml=u"""\
+intercoopVersion: 1.0
+peerVersion: 1
+peerid: somacme
+name: Som Acme, SCCL
+"""
+
+
 class PeerDataStorage_Test(unittest.TestCase):
     
     def setUp(self):
         import os
-        self.peerdatadir = ("peerdatas")
+        self.peerdatadir = "peerdata"
+        self.cleanUp()
         os.system("mkdir -p "+self.peerdatadir)
+        with open(os.path.join(self.peerdatadir, 'somacme.yaml'),'w') as f:
+            f.write(somacmeyaml)
+
+    def tearDown(self):
+        self.cleanUp()
+
+    def cleanUp(self):
+        import shutil
+        try:
+            shutil.rmtree(self.peerdatadir)
+        except: pass
+
 
     def test_get(self):
         s = peerdatastorage.PeerDataStorage(self.peerdatadir)
         peerData = s.get("somacme")
-        self.assertEqual(peerData,ns())
+        self.assertEqual(peerData,ns.loads(somacmeyaml)) # TODO: proper value
 
 # vim: ts=4 sw=4 et
