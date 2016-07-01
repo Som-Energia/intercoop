8eff47ad26cab465089e7f2104f530362aba9942
David García Garzón <david.garcia@upf.edu>
2016-07-01 17:09:20 +0200

    tabs removed

--- python/intercoop/userinfo.py
+++ python/intercoop/userinfo.py
@@ -11,38 +11,38 @@ import os
 
 class BadUser(Exception):
     def __init__(self, user):
-		super(BadUser, self).__init__(
-			"User not found '{}'".format(user))
+        super(BadUser, self).__init__(
+            "User not found '{}'".format(user))
 
 class BadField(Exception):
     def __init__(self, field):
-		super(BadField, self).__init__(
-			"Unrecognized user field '{}'".format(field))
+        super(BadField, self).__init__(
+            "Unrecognized user field '{}'".format(field))
 
 class UserInfo(object):
-	"""This mock object returns user info related to provided keys.
-	In your system the substitute of this object should use the ERP
-	to get such information.
-	"""
+    """This mock object returns user info related to provided keys.
+    In your system the substitute of this object should use the ERP
+    to get such information.
+    """
 
-	def __init__(self, datadir):
-		self.datadir = datadir
+    def __init__(self, datadir):
+        self.datadir = datadir
 
-	def getFields(self, user, fields):
-		try:
-			userdata = ns.load(os.path.join(self.datadir, user+'.yaml'))
-		except Exception:
-			raise BadUser(user)
+    def getFields(self, user, fields):
+        try:
+            userdata = ns.load(os.path.join(self.datadir, user+'.yaml'))
+        except Exception:
+            raise BadUser(user)
 
-		for field in fields:
-			if field not in userdata:
-				raise BadField(field)
+        for field in fields:
+            if field not in userdata:
+                raise BadField(field)
 
-		return ns([
-			(key, value)
-			for key,value in userdata.iteritems()
-			if key in fields
-			])
+        return ns([
+            (key, value)
+            for key,value in userdata.iteritems()
+            if key in fields
+            ])
 
 
 
