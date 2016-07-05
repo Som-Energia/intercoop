876772e99f31d1aecdccc4215f4683c7f2fd7d65
David García Garzón <voki@canvoki.net>
2016-07-05 21:09:35 +0200

    unicoding

--- python/intercoop/translation_test.py
+++ python/intercoop/translation_test.py
@@ -16,18 +16,18 @@ services:
 
 i18n1stlevel=u"""\
 description:
-  es: La cooperativa para atrapar correcaminos
+  es: Mensaje en español
 """
 
 i18nfallback=u"""\
 description:
-  en: The cooperative for catching roadrunners
+  en: Message in English
 """
 
 i18nmanylangs=u"""\
 description:
-  es: La cooperativa para atrapar correcaminos
-  en: The cooperative for catching roadrunners
+  es: Mensaje en español
+  en: Message in English
 """
 
 i18nmanylevels=u"""\
@@ -46,21 +46,21 @@ class Portal_Test(unittest.TestCase):
         self.maxDiff=None
 
     def test_fieldTranslation_existTranslationFirstLevel(self):
-        data = ns.loads(i18n1stlevel)
+        data = ns.loads(i18n1stlevel.encode('utf8'))
         t = translation.Translator()
         self.assertEqual(
-            "La cooperativa para atrapar correcaminos",
+            u"Mensaje en español",
             t.fieldTranslation(data,"description","es"))
 
     def test_fieldTranslation_fallbackTranslation(self):
-        data = ns.loads(i18nfallback)
+        data = ns.loads(i18nfallback.encode('utf8'))
         t = translation.Translator()
         self.assertEqual(
-            "The cooperative for catching roadrunners",
+            "Message in English",
             t.fieldTranslation(data,"description","es","en"))
 
     def test_fieldTranslation_doesntExistFallback(self):
-        data = ns.loads(i18nfallback)
+        data = ns.loads(i18nfallback.encode('utf8'))
         t = translation.Translator()
         with self.assertRaises(Exception) as ctx:
             t.fieldTranslation(data,"description","fr","ca")
@@ -69,14 +69,14 @@ class Portal_Test(unittest.TestCase):
 
 
     def test_fieldTranslation_fallbackLangTranslation(self):
-        data = ns.loads(i18nmanylangs)
+        data = ns.loads(i18nmanylangs.encode('utf8'))
         t = translation.Translator()
         self.assertEqual(
-            "La cooperativa para atrapar correcaminos",
+            u"Mensaje en español",
             t.fieldTranslation(data,"description","es","en"))
 
     def test_fieldTranslation_doesntExistFieldFirstLevel(self):
-        data = ns.loads(i18n1stlevel)
+        data = ns.loads(i18n1stlevel.encode('utf8'))
         t = translation.Translator()
         with self.assertRaises(Exception) as ctx:
             t.fieldTranslation(data,"badfield","es")
@@ -84,7 +84,7 @@ class Portal_Test(unittest.TestCase):
             "Invalid field 'badfield'")
 
     def test_fieldTranslation_doesntExistTranslationFirstLevel(self):
-        data = ns.loads(i18n1stlevel)
+        data = ns.loads(i18n1stlevel.encode('utf8'))
         t = translation.Translator()
         with self.assertRaises(Exception) as ctx:
             t.fieldTranslation(data,"description","fr")
@@ -92,7 +92,7 @@ class Portal_Test(unittest.TestCase):
             "Invalid translation 'fr' for field 'description'")
 
     def test_fieldTranslation_existTranslationManyLevels(self):
-        data = ns.loads(i18nmanylevels)
+        data = ns.loads(i18nmanylevels.encode('utf8'))
         t = translation.Translator()
         self.assertEqual(
             "Yunques garantizados, siempre caen en una cabeza\n",
@@ -107,7 +107,7 @@ class Portal_Test(unittest.TestCase):
             t.translateTree(data,"es"))
 
     def test_translateTree_firstLevel(self):
-        data = ns.loads(i18n1stlevel)
+        data = ns.loads(i18n1stlevel.encode('utf8'))
         t = translation.Translator()
         tree = ns(data)
         tree.description = tree.description.es
@@ -116,7 +116,7 @@ class Portal_Test(unittest.TestCase):
             t.translateTree(data,"es"))
 
     def test_translateTree_manyLevels(self):
-        data = ns.loads(i18nmanylevels)
+        data = ns.loads(i18nmanylevels.encode('utf8'))
         t = translation.Translator()
         tree = ns(data)
         tree.services.anvil.name = tree.services.anvil.name.es
@@ -124,6 +124,10 @@ class Portal_Test(unittest.TestCase):
         self.assertEqual(
             tree,
             t.translateTree(data,"es"))
+
+
+
+
 
 
 # vim: ts=4 sw=4 et
