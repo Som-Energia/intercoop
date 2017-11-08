3f963638b36be4ac7ee8d5ead13072d6d3439078
David García Garzón <voki@canvoki.net>
2017-11-09 00:06:49 +0100

    indentation

--- php/intercoop/tests/CryptoTest.php
+++ php/intercoop/tests/CryptoTest.php
@@ -1,29 +1,29 @@
 <?php 
-use SomLabs\Intercoop\Crypto as crypto;  
+use SomLabs\Intercoop\Crypto as crypto;
 
 class CryptoTest extends PHPUnit_Framework_TestCase{
 
 	private $plain = "this is the content\n";
 	private $base64 = "dGhpcyBpcyB0aGUgY29udGVudAo=";
 	private $signed = (
-        "AxmEUIQBd82wC4-9Jm337gWbvMapcLMVvE3Ord9wvnFmvuMUW7qzO-uI8Iac".
-        "rW6uPWM-93g9Y6q2YjfeQCZl_JB7lJorY5PLgSXhvu0-TcCPFkaIEAh7-4Tl".
-        "lQx_-hwoN1Q3REOy-pB12iJZf9XrrOejfGG83kqXmXElSeS5RAWKwt2FcJFL".
-        "IZIRZ9CDHRvX31428YURv-HlmpklwBE_t6WSJmc-b4dCcTDKih-eJ3OteDvM".
-        "csN_0H76uzEZTbJf3GwH8m5lCjNkWKVufBP_J2aQ-LvtgKiuyZI6lP9TcffV".
-        "da9k4vdM2zoPDtGTAxZQz68suevbGbAM_fYnBge2FA=="
-        );
+		"AxmEUIQBd82wC4-9Jm337gWbvMapcLMVvE3Ord9wvnFmvuMUW7qzO-uI8Iac".
+		"rW6uPWM-93g9Y6q2YjfeQCZl_JB7lJorY5PLgSXhvu0-TcCPFkaIEAh7-4Tl".
+		"lQx_-hwoN1Q3REOy-pB12iJZf9XrrOejfGG83kqXmXElSeS5RAWKwt2FcJFL".
+		"IZIRZ9CDHRvX31428YURv-HlmpklwBE_t6WSJmc-b4dCcTDKih-eJ3OteDvM".
+		"csN_0H76uzEZTbJf3GwH8m5lCjNkWKVufBP_J2aQ-LvtgKiuyZI6lP9TcffV".
+		"da9k4vdM2zoPDtGTAxZQz68suevbGbAM_fYnBge2FA=="
+		);
 	private $shahex = "a567f5414a110729344c395089d87821310b22f4";
 	
 
 	function test_encode_unicode(){
 		$encoded = crypto::encode($this->plain);
-        	$this->assertEquals($this->base64, $encoded);		
+		$this->assertEquals($this->base64, $encoded);
 	}
 
 	function test_decode_unicode(){
 		$decoded = crypto::decode($this->base64);
-        	$this->assertEquals($this->plain, $decoded);		
+		$this->assertEquals($this->plain, $decoded);
 	}
 
 	function TODO_test_decode_incorrectBase64Padding(){
@@ -52,4 +52,4 @@ class CryptoTest extends PHPUnit_Framework_TestCase{
 
 }
 
-// vim: noet ts=4
+// vim: noet ts=4 sw=4
