add8237acc0f2da4a951e173c86270f61ff0ab74
David García Garzón <voki@canvoki.net>
2017-11-08 13:16:20 +0100

    Restricted scope to test members and methods

--- php/intercoop/tests/CryptoTest.php
+++ php/intercoop/tests/CryptoTest.php
@@ -3,9 +3,9 @@ use SomLabs\Intercoop\Crypto as crypto;
 
 class CryptoTest extends PHPUnit_Framework_TestCase{
 
-	public $plain = "this is the content\n";
-	public $base64 = "dGhpcyBpcyB0aGUgY29udGVudAo=";
-	public $signed = (
+	private $plain = "this is the content\n";
+	private $base64 = "dGhpcyBpcyB0aGUgY29udGVudAo=";
+	private $signed = (
         "AxmEUIQBd82wC4-9Jm337gWbvMapcLMVvE3Ord9wvnFmvuMUW7qzO-uI8Iac".
         "rW6uPWM-93g9Y6q2YjfeQCZl_JB7lJorY5PLgSXhvu0-TcCPFkaIEAh7-4Tl".
         "lQx_-hwoN1Q3REOy-pB12iJZf9XrrOejfGG83kqXmXElSeS5RAWKwt2FcJFL".
@@ -16,14 +16,23 @@ class CryptoTest extends PHPUnit_Framework_TestCase{
 	
 
 	
-	public function test_encode_unicode(){
+	function test_encode_unicode(){
 		$encoded = crypto::encode($this->plain);
         	$this->assertEquals($this->base64, $encoded);		
 	}
 
-	public function test_decode_unicode(){
+	function test_decode_unicode(){
 		$decoded = crypto::decode($this->base64);
         	$this->assertEquals($this->plain, $decoded);		
 	}
-  
+
+	function todo_test_decode_incorrectBase64Padding(){
+		try {
+			crypto::decode("AA");
+			$this->fail("Exception not thrown");
+		} catch (Exception $e) {
+			$this->assertEquals($e.errorMessage(), "Incorrect Padding");
+		}
+	}
+
 }
