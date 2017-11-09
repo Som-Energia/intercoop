f2175728c1b3455f3318be6c357f303de60d86c7
David García Garzón <voki@canvoki.net>
2017-11-09 10:56:18 +0100

    signing things

--- php/intercoop/src/Crypto.php
+++ php/intercoop/src/Crypto.php
@@ -1,20 +1,77 @@
 <?php 
 namespace SomLabs\Intercoop;
 
+use phpseclib\Crypt\RSA;
+
 class Crypto{
 
 	/// Encode text into base64 (as text)
 	static function encode($text){
-		return base64_encode($text);
+		if (!\mb_check_encoding($text, 'UTF-8'))
+			throw new \Exception('Not UTF-8 unicode');
+		return self::bencode($text);
 	}
 
 	/// Decode base64 (as text) back into text
 	static function decode($b64string){
-		return base64_decode($b64string);
+		$text = self::bdecode($b64string);
+		if (!\mb_check_encoding($text, 'UTF-8'))
+			throw new \Exception('Not UTF-8 unicode');
+		return $text;
+	}
+
+	/// Encode bytes (binary) into base64 (as text)
+	static function bencode($text){
+		$encoded = base64_encode($text);
+		$encoded = strtr($encoded,'+/','-_'); // TODO: Untested
+		return $encoded;
+	}
+
+	// Decode base64 (as text) back into bytes (binary)
+	static function bdecode($b64string){
+		$b64string = strtr($b64string, '-_', '+/'); // TODO: Untested
+		$decoded = base64_decode($b64string);
+		return $decoded;
 	}
 
 	static function sha($text){
 		return sha1($text);
+	}
+
+	/// Generates a public/private key pair
+	static function generateKeys($filename, $publicFilename){
+		$rsa = new RSA();
+		$key = $rsa->createKey(2048);
+		if ($filename)
+			file_put_contents($filename, $key['privatekey']);
+		if ($publicFilename)
+			file_put_contents($publicFilename, $key['publickey']);
+		return $rsa;
+	}
+
+	static function loadKey($filename){
+		$rsa = new RSA();
+		$rsa->loadKey(file_get_contents($filename));
+		return $rsa;
+	}
+
+	static function sign($privateKey, $text){
+		if (! $privateKey->getPrivateKey())
+			throw new \Exception("No private key available");
+
+		$privateKey->setSignatureMode(RSA::SIGNATURE_PKCS1);
+		$signature = $privateKey->sign($text);
+		$encodedSignature = self::bencode($signature);
+		return $encodedSignature;
+	}
+
+	/// 
+	static function isAuthentic($publickey, $text, $signature){
+		if (!$publickey->getPublicKey()) {
+			throw new \Exception("No public key available");
+		}
+		$publickey->setSignatureMode(RSA::SIGNATURE_PKCS1);
+		return $publickey->verify($text,self::bdecode($signature));
 	}
 }
 
--- php/intercoop/tests/CryptoTest.php
+++ php/intercoop/tests/CryptoTest.php
@@ -1,11 +1,12 @@
 <?php 
 use SomLabs\Intercoop\Crypto as crypto;
+use PHPUnit\Framework\TestCase;
 
 class CryptoTest extends PHPUnit_Framework_TestCase{
 
-	private $plain = "this is the content\n";
-	private $base64 = "dGhpcyBpcyB0aGUgY29udGVudAo=";
-	private $signed = (
+	protected $plain = "this is the content\n";
+	protected $base64 = "dGhpcyBpcyB0aGUgY29udGVudAo=";
+	protected $signed = (
 		"AxmEUIQBd82wC4-9Jm337gWbvMapcLMVvE3Ord9wvnFmvuMUW7qzO-uI8Iac".
 		"rW6uPWM-93g9Y6q2YjfeQCZl_JB7lJorY5PLgSXhvu0-TcCPFkaIEAh7-4Tl".
 		"lQx_-hwoN1Q3REOy-pB12iJZf9XrrOejfGG83kqXmXElSeS5RAWKwt2FcJFL".
@@ -13,43 +14,111 @@ class CryptoTest extends PHPUnit_Framework_TestCase{
 		"csN_0H76uzEZTbJf3GwH8m5lCjNkWKVufBP_J2aQ-LvtgKiuyZI6lP9TcffV".
 		"da9k4vdM2zoPDtGTAxZQz68suevbGbAM_fYnBge2FA=="
 		);
-	private $shahex = "a567f5414a110729344c395089d87821310b22f4";
-	
+	protected $shahex = "a567f5414a110729344c395089d87821310b22f4";
+	protected $keyfile = "testkey.pem";
+	protected $pubfile = "testkey-public.pem";
 
-	function test_encode_unicode(){
+	public function assertExceptionMessage($e, $expected){
+		$this->assertEquals($expected, $e->getMessage());
+	}
+
+	public function setUp(){
+		//crypto::generateKeys($this->keyfile, $this->pubfile);
+		$this->key = crypto::loadKey($this->keyfile);
+		$this->public = crypto::loadKey($this->pubfile);
+	}
+
+	public function test_encode_unicode(){
 		$encoded = crypto::encode($this->plain);
 		$this->assertEquals($this->base64, $encoded);
 	}
 
-	function test_decode_unicode(){
+	public function test_decode_unicode(){
 		$decoded = crypto::decode($this->base64);
 		$this->assertEquals($this->plain, $decoded);
 	}
 
-	function TODO_test_decode_incorrectBase64Padding(){
+	public function TODO_test_decode_incorrectBase64Padding(){
 		try {
 			crypto::decode("AA");
 			$this->fail("Exception not thrown");
 		} catch (Exception $e) {
-			$this->assertEquals($e.errorMessage(), "Incorrect Padding");
+			$this->assertExceptionMessage($e, "Incorrect Padding");
 		}
 	}
-	function TODO_test_decode_notUnicode(){
+	public function test_decode_notUnicode(){
+		$encoded = "ABCD";
 		try {
-			crypto::decode("ABCD");
+			crypto::decode($encoded);
 			$this->fail("Exception not thrown");
 		} catch (Exception $e) {
-			$this->assertEquals($e.errorMessage(), "Not UTF-8 unicode");
+			$this->assertExceptionMessage($e, "Not UTF-8 unicode");
 		}
 	}
 
-	function test_sha(){
-		$this->assertEquals(
-			crypto::sha($this->plain),
-			$this->shahex);
+	public function test_encode_notUnicode(){
+		$binary = "\x00\x01\x83";
+		try {
+			crypto::encode($binary);
+			$this->fail("Exception not thrown");
+		} catch (Exception $e) {
+			$this->assertExceptionMessage($e,"Not UTF-8 unicode");
+		}
 	}
 
+	public function test_bencode() {
+		$binary = "\x01\x00\x02";
+		$expected = "AQAC";
+		$result = crypto::bencode($binary);
+		$this->assertEquals($expected, $result);
+	}
 
+	public function test_bdecode() {
+		$encoded = "AQAC";
+		$expected = "\x01\x00\x02";
+		$result = crypto::bdecode($encoded);
+		$this->assertEquals($expected, $result);
+	}
+
+	public function test_sha(){
+		$sha = crypto::sha($this->plain);
+		$this->assertEquals($sha, $this->shahex);
+	}
+
+	public function test_sign(){
+		$signature = crypto::sign($this->key, $this->plain);
+		$this->assertEquals($signature, $this->signed);
+	}
+
+	public function test_sign_withNoPrivate_fails(){
+		try {
+			crypto::sign($this->public, $this->plain);
+			$this->fail("Exception not thrown");
+		} catch (Exception $e) {
+			$this->assertExceptionMessage($e, "No private key available");
+		}
+	}
+
+	public function test_isAuthentic_whenOk(){
+		$result = crypto::isAuthentic($this->public, $this->plain, $this->signed);
+		$this->assertTrue($result);
+	}
+
+	public function test_isAuthentic_whenPayloadChanged(){
+		$badPayload = "this is NOT the content\n";
+		$result = crypto::isAuthentic($this->public, $badPayload, $this->signed);
+		$this->assertFalse($result);
+	}
+
+	public function test_isAuthentic_withNoPublic_fails(){
+		$key = new phpseclib\Crypt\RSA();
+		try {
+			$result = crypto::isAuthentic($key, $this->plain, $this->signed);
+			$this->fail("Exception not thrown");
+		} catch (Exception $e) {
+			$this->assertExceptionMessage($e, "No public key available");
+		}
+	}
 }
 
 // vim: noet ts=4 sw=4
