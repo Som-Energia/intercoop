e604b6101eec958d89aa50ce45d362e8fe327dc5
David García Garzón <voki@canvoki.net>
2017-11-09 16:39:15 +0100

    Packaging::generate

--- php/intercoop/src/Packaging.php
+++ php/intercoop/src/Packaging.php
@@ -2,9 +2,22 @@
 namespace SomLabs\Intercoop;
 
 use SomLabs\Intercoop\Crypto as crypto;
+use Symfony\Component\Yaml\Yaml;
 
-class Packaging{
+class Packaging {
 
+	# TODO: take it from somewhere else
+	public static $protocolVersion = '1.0';
+
+	static function generate($ownKey, $data){
+		$payload = Yaml::dump($data);
+		$result = array(
+			'intercoopVersion' => self::$protocolVersion,
+			'payload' => crypto::encode($payload),
+			'signature' => crypto::sign($ownKey, $payload),
+		);
+		return Yaml::dump($result);
+	}
 
 }
 
--- php/intercoop/tests/PackagingTest.php
+++ php/intercoop/tests/PackagingTest.php
@@ -1,17 +1,62 @@
 <?php 
-use SomLabs\Intercoop\Crypto as crypto;
 use PHPUnit\Framework\TestCase;
+use SomLabs\Intercoop\Packaging as packaging;
+use SomLabs\Intercoop\Crypto as crypto;
+use Symfony\Component\Yaml\Yaml;
 
-class PackagingTest extends PHPUnit\Framework\TestCase{
 
-	public function assertExceptionMessage($e, $expected){
+class KeyRingMock {
+	function __construct($keymap) {
+	}
+}
+
+class PackagingTest extends PHPUnit_Framework_TestCase{
+
+	protected $keyfile = "testkey.pem";
+	protected $pubfile = "testkey-public.pem";
+
+	protected $yaml = <<<EOT
+intercoopVersion: '1.0'
+originpeer: testpeer
+origincode: 666
+name: Perico de los Palotes
+address: Percebe, 13
+city: Villarriba del Alcornoque
+state: Albacete
+postalcode: '01001'
+country: ES
+EOT;
+
+
+	public function assertExceptionMessage($e, $expected) {
 		$this->assertEquals($expected, $e->getMessage());
 	}
 
-	public function setUp(){
+	public function setUp() {
 		//crypto::generateKeys($this->keyfile, $this->pubfile);
 		$this->key = crypto::loadKey($this->keyfile);
 		$this->public = crypto::loadKey($this->pubfile);
+
+		$this->values = Yaml::parse($this->yaml);
+		// Required redump to sort elements
+		$this->yaml = Yaml::dump($this->values);
+		$this->encodedPayload1 = crypto::encode($this->yaml);
+		$this->signedPayload1 = crypto::sign($this->key, $this->yaml);
+		$this->keyring = new KeyRingMock(array(
+			'testpeer' => $this->public,
+			));
+	}
+
+	public function test_generate() {
+		$message = packaging::generate($this->key, $this->values);
+		
+		$this->assertEquals(
+			Yaml::parse($message),
+			array(
+				'intercoopVersion' => '1.0',
+				'signature' => $this->signedPayload1,
+				'payload' => $this->encodedPayload1,
+			));
 	}
 
 }
